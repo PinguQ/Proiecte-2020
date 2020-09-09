047672babab90fc82102ced8b3038e7a9ef97df8
PinguQ <71019033+PinguQ@users.noreply.github.com>
2020-09-09 15:57:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+Biblioteca iostream: cin>>citire
+cout<<afisare
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;//initializam variabila
+    cout<<"Dati-l pe a:"<<endl;
+    cin>>a;//Citim de la tastatura pe a
+    cout<<"a = "<<a;//Il afisam pe ecran pe a
+    return 0;
+}
